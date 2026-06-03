d65fd44142b804f87503acef0649c534a1a68bcb
Bruna Szarin <brunaszarin@gmail.com>
2026-06-03 20:15:11 -0300

    chore: add spring boot project to backend

---
+++ backend/src/test/java/com/financetracker/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financetracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
